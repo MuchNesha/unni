74197e210b79af98ed2f3d42148c828bff54e22d
bahtiar adi prasetya <bahtiaradi62@gmail.com>
2019-12-08 14:32:16 +0700

    Update footer.php

--- application/views/footer.php
+++ application/views/footer.php
@@ -2,32 +2,69 @@
             <div class="container">
                 <div class="row">
                     <!-- Single Footer Area Start -->
-                    <div class="col-12 col-md-6 col-lg-3">
+                   <!-- <div class="col-12 col-md-6 col-lg-3">
                         <div class="single_footer_area">
                             <div class="footer-logo">
                                 <img src="img/core-img/logo.png" alt="">
                             </div>
                             <div class="copywrite_text d-flex align-items-center">
                                 <p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | Made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a> &amp; distributed by <a href="https://themewagon.com" target="_blank">ThemeWagon</a>
+<!--Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | Made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a> &amp; distributed by <a href="https://themewagon.com" target="_blank">ThemeWagon</a>
 <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
-                            </div>
+                          <!--  </div>
+                        </div>
+                    </div>-->
+                    <!-- Single Footer Area Start -->
+                    <div class="col-md-4 pt-3">
+                    <h4 style="font-family: 'Nunito' ;  font-size: 25px;">Unni Korean Grup</h4>
+                    <div class="col-md-7 pt-2">
+                        <div class="single_footer_area">
+                            <ul class="footer_widget_menu">
+                                <li><a href="#">Unni Korean Food</a></li>
+                                <li><a href="#">Unni Korean Rent Car</a></li>
+                                <li><a href="#">Unni korean Fashion</a></li>
+                            </ul>
                         </div>
                     </div>
+                    </div>
                     <!-- Single Footer Area Start -->
-                    <div class="col-12 col-sm-6 col-md-3 col-lg-2">
+                    <div class="col-4 md-4 pt-3">
+                    <h4 style="font-family: 'Nunito' ;  font-size: 25px;">Unni Korean Fashion</h4>
+                    <div class="col-md-7 pt-2">
+                        <div class="single_footer_area">
+                            <ul class="footer_widget_menu">
+                                <li><a href="#">Tentang</a></li>
+                                <li><a href="#">Kotak kami</a></li>
+                                <li><a href="#">Kebijakan pengguna</a></li>
+                                <li><a href="#">sales</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    </div>
+                    <div class="col-md-4 pt-3 float-right">
+                    <h4 style="font-family: 'Nunito' ;  font-size: 25px;">Social Media</h4>
+                    <div class="col-md-4 pt-2">
                         <div class="single_footer_area">
                             <ul class="footer_widget_menu">
                                 <li><a href="#">About</a></li>
                                 <li><a href="#">Blog</a></li>
-                                <li><a href="#">Faq</a></li>
-                                <li><a href="#">Returns</a></li>
-                                <li><a href="#">Contact</a></li>
                             </ul>
                         </div>
                     </div>
-                    <!-- Single Footer Area Start -->
-                    <div class="col-12 col-sm-6 col-md-3 col-lg-2">
+                    </div>
+                    <div class="col-12 col-md-12 col-lg-3">
+                        <div class="single_footer_area">
+                            <div class="footer-logo">
+                                <img src="img/core-img/logo.png" alt="">
+                            </div>
+                            <div class="copywrite_text d-flex align-items-center">
+                                <p>
+Copyright &copy;<script>document.write(new Date().getFullYear());</script> | <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="#" target="_blank">Crutu Tech</a> & <a href="https://colorlib.com" target="_blank">Colorlib</a>
+</p>
+                           </div>
+                        </div>
+                    </div>
+                    <!--<div class="col-12 col-sm-6 col-md-3 col-lg-2">
                         <div class="single_footer_area">
                             <ul class="footer_widget_menu">
                                 <li><a href="#">My Account</a></li>
@@ -37,8 +74,8 @@ Copyright &copy;<script>document.write(new Date().getFullYear());</script> All r
                             </ul>
                         </div>
                     </div>
-                    <!-- Single Footer Area Start -->
-                    <div class="col-12 col-lg-5">
+                     Single Footer Area Start -->
+                   <!-- <div class="col-12 col-lg-5">
                         <div class="single_footer_area">
                             <div class="footer_heading mb-30">
                                 <h6>Subscribe to our newsletter</h6>
@@ -51,5 +88,5 @@ Copyright &copy;<script>document.write(new Date().getFullYear());</script> All r
                             </div>
                         </div>
                     </div>
-                </div>
+                </!-->
                 <div class="line"></div>

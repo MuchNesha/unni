90c8838cccaf953147d0b67baa82633aef5cfcd0
waragafila <ajicheater@gmail.com>
2019-12-12 21:02:26 +0700

    add

---
+++ application/views/kontakkami/kontak.php
@@ -0,0 +1,65 @@
+        <!-- ****** Top Discount Area Start ****** -->
+        <section class="top-discount-area d-md-flex align-items-center">
+            <!-- Single Discount Area -->
+            <div class="single-discount-area">
+                <h5>Bebas Ongkir</h5>
+                <h6><a href="#">Untuk Wilayah Bondowoso</a></h6>
+            </div>
+            <!-- Single Discount Area -->
+            <div class="single-discount-area">
+                <h5>Dapatkan Diskon 50%</h5>
+                <h6>Selama Bulan Ramadhan</h6>
+            </div>
+            <!-- Single Discount Area -->
+            <div class="single-discount-area">
+                <h5>Dapatkan Diskon 20%</h5>
+                <h6>Untuk Mukena</h6>
+            </div>
+        </section>
+        <!-- ****** Top Discount Area End ****** -->
+
+        <!-- ****** Welcome Slides Area Start ****** -->
+        <section class="welcome_area">
+                <!-- Single Slide Start -->
+                <div class="single_slide height-500 bg-img background-overlay" style="background-image: url(<?php echo base_url(); ?>/assets/img/bg-img/bg-1.jpg);">
+                    <div class="container h-100">
+                        <div class="row h-100 align-items-center">
+                            <div class="col-12">
+                                <div class="welcome_slide_text">
+                                    <h2 data-animation="fadeInUp" data-delay="500ms" data-duration="500ms">Kontak Kami</h2>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+ <!-- ****** New Arrivals Area Start ****** -->
+ <br>
+ <br>
+ <br>
+            <div class="cart-page-heading">
+            <h1>Kontak Kami</h1>
+           <p><span>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          <!-- <img class="kanan" src="<?php echo base_url(); ?>/assets/img/bg-img/kontakkami.png"  height="100px" top="10px" align="right" /> -->
+           Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+           Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+           Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+           </div>
+
+        <!-- ****** Offer Area Start ****** -->
+        <section class="offer_area height-70 section_padding_100 bg-img" style="background-image: url(<?php echo base_url(); ?>/assets/img/bg-img/kontak.png);">
+        </section>
+        <!-- ****** Offer Area End ****** -->
+    <!-- jQuery (Necessary for All JavaScript Plugins) -->
+    <script src="<?php echo base_url(); ?>/assets/js/jquery/jquery-2.2.4.min.js"></script>
+    <!-- Popper js -->
+    <script src="<?php echo base_url(); ?>/assets/js/popper.min.js"></script>
+    <!-- Bootstrap js -->
+    <script src="<?php echo base_url(); ?>/assets/js/bootstrap.min.js"></script>
+    <!-- Plugins js -->
+    <script src="<?php echo base_url(); ?>/assets/js/plugins.js"></script>
+    <!-- Active js -->
+    <script src="<?php echo base_url(); ?>/assets/js/active.js"></script>
+
+</body>
+
+</html>

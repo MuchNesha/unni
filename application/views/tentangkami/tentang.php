8f42f467af8ab8b6e238219d0cf28dc4585ee8ac
waragafila <ajicheater@gmail.com>
2019-12-12 19:36:56 +0700

    tentang

---
+++ application/views/tentangkami/tentang.php
@@ -0,0 +1,60 @@
+        <!-- ****** Top Discount Area Start ****** -->
+        <section class="top-discount-area d-md-flex align-items-center">
+            <!-- Single Discount Area -->
+            <div class="single-discount-area">
+                <h5>Bebas Ongkir</h5>
+                <h6><a href="#">Untuk Wilayah Bondowoso</a></h6>
+            </div>
+            <!-- Single Discount Area -->
+            <div class="single-discount-area">
+                <h5>Dapatkan Diskon 50%</h5>
+                <h6>Selama Bulan Ramadhan</h6>
+            </div>
+            <!-- Single Discount Area -->
+            <div class="single-discount-area">
+                <h5>Dapatkan Diskon 20%</h5>
+                <h6>Untuk Mukena</h6>
+            </div>
+        </section>
+        <!-- ****** Top Discount Area End ****** -->
+
+        <!-- ****** Welcome Slides Area Start ****** -->
+        <section class="welcome_area">
+                <!-- Single Slide Start -->
+                <div class="single_slide height-800 bg-img background-overlay" style="background-image: url(<?php echo base_url(); ?>/assets/img/bg-img/bg-1.jpg);">
+                    <div class="container h-100">
+                        <div class="row h-100 align-items-center">
+                            <div class="col-12">
+                                <div class="welcome_slide_text">
+                                    <h2 data-animation="fadeInUp" data-delay="500ms" data-duration="500ms">Tentang Kami</h2>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+ <!-- ****** New Arrivals Area Start ****** -->
+ <section class="new_arrivals_area section_padding_100_0 clearfix">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="section_heading text-center">
+                            <h2>Tentang Kami</h2>
+                        </div>
+                    </div>
+                </div>
+            </div>
+ 
+    <!-- jQuery (Necessary for All JavaScript Plugins) -->
+    <script src="<?php echo base_url(); ?>/assets/js/jquery/jquery-2.2.4.min.js"></script>
+    <!-- Popper js -->
+    <script src="<?php echo base_url(); ?>/assets/js/popper.min.js"></script>
+    <!-- Bootstrap js -->
+    <script src="<?php echo base_url(); ?>/assets/js/bootstrap.min.js"></script>
+    <!-- Plugins js -->
+    <script src="<?php echo base_url(); ?>/assets/js/plugins.js"></script>
+    <!-- Active js -->
+    <script src="<?php echo base_url(); ?>/assets/js/active.js"></script>
+
+</body>
+
+</html>

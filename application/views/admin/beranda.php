f98aba8677601e8523b48a22784483016c74e863
waragafila <ajicheater@gmail.com>
2019-12-12 07:45:20 +0700

    --

--- application/views/admin/beranda.php
+++ application/views/admin/beranda.php
@@ -27,7 +27,7 @@
       </button>
       <!-- Brand -->
       <a class="navbar-brand pt-0" href="./index.html">
-        <img src="<?php echo base_url(); ?>/assets/img/brand/blue.png" class="navbar-brand-img" alt="...">
+        <img src="<?php echo base_url(); ?>/assets/img/core-img/logo.png" class="navbar-brand-img" alt="...">
       </a>
       <!-- User -->
       <ul class="nav align-items-center d-md-none">
@@ -115,7 +115,7 @@
         <!-- Navigation -->
         <ul class="navbar-nav">
           <li class="nav-item  class=" active" ">
-          <a class=" nav-link active " href=" <?php echo base_url(); ?>/index.html"> <i class="ni ni-tv-2 text-primary"></i> Dashboard
+          <a class=" nav-link active " href=" <?php echo base_url(); ?>/index.html"> <i class="ni ni-tv-2 text-primary"></i> Beranda
             </a>
           </li>
           <li class="nav-item">
@@ -146,28 +146,6 @@
           <li class="nav-item">
             <a class="nav-link" href="<?php echo base_url(); ?>/examples/register.html">
               <i class="ni ni-circle-08 text-pink"></i> Register
-            </a>
-          </li>
-        </ul>
-        <!-- Divider -->
-        <hr class="my-3">
-        <!-- Heading -->
-        <h6 class="navbar-heading text-muted">Documentation</h6>
-        <!-- Navigation -->
-        <ul class="navbar-nav mb-md-3">
-          <li class="nav-item">
-            <a class="nav-link" href="https://demos.creative-tim.com/argon-dashboard/docs/getting-started/overview.html">
-              <i class="ni ni-spaceship"></i> Getting started
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="https://demos.creative-tim.com/argon-dashboard/docs/foundation/colors.html">
-              <i class="ni ni-palette"></i> Foundation
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="https://demos.creative-tim.com/argon-dashboard/docs/components/alerts.html">
-              <i class="ni ni-ui-04"></i> Components
             </a>
           </li>
         </ul>

e227b84e1702529b6eee9438bda306cac0959be9
much nesha adinata r <41299486+MuchNesha@users.noreply.github.com>
2019-12-19 10:34:04 +0700

    Update konfirmasi.php

--- application/views/admin/konfirmasi.php
+++ application/views/admin/konfirmasi.php
@@ -36,66 +36,64 @@
                     <th scope="col">Total Bayar</th>
                     <th scope="col">Rincian</th>
                     <th scope="col">Keputusan</th>
-                    <th scope="col"></th>
                   </tr>
                 </thead>
+
+                <!-- Modal -->
+                <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                  <div class="modal-dialog modal-dialog-centered" role="document">
+                    <div class="modal-content">
+                      <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">Rincian ID Pesanan : </h5>
+                          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                          </button>
+                      </div>
+                      <div class="modal-body">
+                      <b> <h3>Info Transfer</h3> <br> </b>
+                      <h4>ID Pesanan :</h4>
+                      <h5>$var</h5>
+                      <
+                      <h4>Tanggal :</h4>
+                      <h5>$VAR</h5>
+
+                      </div>
+                      <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Tutup</button>
+                        <button type="button" class="btn btn-primary">Setujui</button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
                 <tbody>
                   <tr>
                     <th scope="row">
                       <div class="media align-items-center">
-                        <a href="#" class="avatar rounded-circle mr-3">
-                          <img alt="Image placeholder" src="../assets/img/theme/vue.jpg">
-                        </a>
                         <div class="media-body">
-                          <span class="mb-0 text-sm">Vue Paper UI Kit PRO</span>
+                          <span class="mb-0 text-sm">
+                            111
+                          </span>
                         </div>
                       </div>
                     </th>
                     <td>
-                      $2,200 USD
+                    <span class="mb-0 text-sm">
+                      11111111
+                    </span>
                     </td>
                     <td>
-                      <span class="badge badge-dot mr-4">
-                        <i class="bg-success"></i> completed
+                      <span class="mb-0 text-sm">
+                        completed
                       </span>
                     </td>
                     <td>
-                      <div class="avatar-group">
-                        <a href="#" class="avatar avatar-sm" data-toggle="tooltip" data-original-title="Ryan Tompson">
-                          <img alt="Image placeholder" src="../assets/img/theme/team-1-800x800.jpg" class="rounded-circle">
-                        </a>
-                        <a href="#" class="avatar avatar-sm" data-toggle="tooltip" data-original-title="Romina Hadid">
-                          <img alt="Image placeholder" src="../assets/img/theme/team-2-800x800.jpg" class="rounded-circle">
-                        </a>
-                        <a href="#" class="avatar avatar-sm" data-toggle="tooltip" data-original-title="Alexander Smith">
-                          <img alt="Image placeholder" src="../assets/img/theme/team-3-800x800.jpg" class="rounded-circle">
-                        </a>
-                        <a href="#" class="avatar avatar-sm" data-toggle="tooltip" data-original-title="Jessica Doe">
-                          <img alt="Image placeholder" src="../assets/img/theme/team-4-800x800.jpg" class="rounded-circle">
-                        </a>
-                      </div>
+                    <button type="button" class="btn btn-secondary" data-toggle="modal" data-target="#exampleModal">
+                    Rincian
+                    </button>
+
                     </td>
                     <td>
-                      <div class="d-flex align-items-center">
-                        <span class="mr-2">100%</span>
-                        <div>
-                          <div class="progress">
-                            <div class="progress-bar bg-success" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 100%;"></div>
-                          </div>
-                        </div>
-                      </div>
-                    </td>
-                    <td class="text-right">
-                      <div class="dropdown">
-                        <a class="btn btn-sm btn-icon-only text-light" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                          <i class="fas fa-ellipsis-v"></i>
-                        </a>
-                        <div class="dropdown-menu dropdown-menu-right dropdown-menu-arrow">
-                          <a class="dropdown-item" href="#">Action</a>
-                          <a class="dropdown-item" href="#">Another action</a>
-                          <a class="dropdown-item" href="#">Something else here</a>
-                        </div>
-                      </div>
+                      <button type="button" class="btn btn-primary">Setujui</button>
                     </td>
                   </tr>
                 </tbody>

68570819aa101dd4ed2c99de3f37bd88ed5af9b2
much nesha adinata r <41299486+MuchNesha@users.noreply.github.com>
2019-12-19 17:48:10 +0700

    Update tambahbarang.php

--- application/views/admin/tambahbarang.php
+++ application/views/admin/tambahbarang.php
@@ -1,95 +1,91 @@
 <?php $this->load->view('admin/partials/head.php'); ?>
 <?php $this->load->view('admin/partials/menu.php'); ?>
-  <div class="main-content">
-            <!-- Page content -->
-            <div class="header bg-gradient-primary pb-8 pt-5 pt-md-8">
+    <div class="main-content">
+        <!-- Page content -->
+        <div class="header bg-gradient-primary pb-8 pt-5 pt-md-8">
             <div class="container-fluid">
                 <div class="header-body">
                     <h1 style="color:aliceblue">Tambah Barang</h1>
                         <br>
                         <h3 style="color:aliceblue">Admin bisa menambahkan barang dihalaman ini 😉</h3>
-                        <br> <br>
+                        <br>
                     <!-- Start Container Fluid -->
-          <div class="card bg-secondary shadow">
-            <div class="card-header bg-white border-0">
-              <div class="row align-items-center">
-                <div class="col-8">
-                  <h3 class="mb-0">Tambah Barang</h3>
-                </div>
-                <div class="col-4 text-right">
-                <input class="btn btn-primary" type="submit" name="btn" value="Simpan" />
-                                        <br />
-                                        <?php if ($this->session->flashdata('success')) : ?>
+                    <div class="card bg-secondary shadow">
+                        <div class="card-header bg-white border-0">
+                            <div class="row align-items-center">
+                                <div class="col-8">
+                                <h3 class="mb-0">Tambah Barang</h3>
+                                </div>
+                                    <div class="col-4 text-right">
+                                    <input class="btn btn-primary" type="submit" name="btn" value="Simpan" />
+                                    <br />
+                                    <?php if ($this->session->flashdata('success')) : ?>
                                         <div class="alert alert-success" role="alert">
                                             <?php echo $this->session->flashdata('success'); ?>
                                         </div>
                                         <?php endif; ?>
-                </div>
-              </div>
-            </div>
+                                    </div>
+                            </div>
+                        </div>
 
                             <div class="card-body">
-                                    <form action="<?php base_url('admin/databarang/add') ?>" method="post"
-                                        enctype="multipart/form-data">
-                                        <div class="row">
-                                            <div class="col-md-6">
-                                                <div class="form-group">
-                                                    <label for="id_barang">Nama Barang</label>
-                                                    <input class="form-control" type="text" name="id_barang"
-                                                        placeholder="Nama Barang" />
-                                                </div>
+                                <form action="<?php base_url('admin/databarang/add') ?>" method="post" enctype="multipart/form-data">
+                                    <div class="row">
+                                        <div class="col-md-6">
+                                            <div class="form-group">
+                                                <label for="id_barang">Nama Barang</label>
+                                                <input class="form-control" type="text" name="id_barang" placeholder="Nama Barang" />
                                             </div>
-                                            <div class="col-md-6">
-                                                <div class="form-group">
-                                                    <label for="form-control">Pilih kategori Barang</label>
-                                                    <select class="form-control" id="kategoribarang">
-                                                    <option>Hijab Instant</option>
-                                                    <option>Hijab Segiempat</option>
-                                                    <option>Baju Muslim</option>
-                                                    <option>Aksesoris</option>
-                                                    </select>
-                                                </div>
+                                        </div>
+                                        <div class="col-md-6">
+                                            <div class="form-group">
+                                                <label for="form-control">Pilih kategori Barang</label>
+                                                <select class="form-control" id="kategoribarang">
+                                                <option>Hijab Instant</option>
+                                                <option>Hijab Segiempat</option>
+                                                <option>Baju Muslim</option>
+                                                <option>Aksesoris</option>
+                                                </select>
                                             </div>
-                                            <div class="col-md-6">
-                                                <div class="form-group">
-                                                    <label for="harga">Harga Barang</label>
-                                                    <input class="form-control" type="number" name="harga" placeholder="Rp." />
-                                                </div>
+                                        </div>
+                                        <div class="col-md-6">
+                                            <div class="form-group">
+                                                <label for="harga">Harga Barang</label>
+                                                <input class="form-control" type="number" name="harga" placeholder="Rp." />
                                             </div>
-                                            <div class="col-md-3">
-                                                <div class="form-group">
-                                                    <label for="grosir">Harga Grosir Barang</label>
-                                                    <input class="form-control" type="number" name="grosir" placeholder="Rp." />
-                                                </div>
+                                        </div>
+                                        <div class="col-md-3">
+                                            <div class="form-group">
+                                                <label for="grosir">Harga Grosir Barang</label>
+                                                <input class="form-control" type="number" name="grosir" placeholder="Rp." />
                                             </div>
+                                        </div>
                                             <div class="col-md-3">
                                                 <div class="form-group">
                                                     <label for="grosir">Per</label>
                                                     <input class="form-control" type="number" name="qty" placeholder="QTY" />
                                                 </div>
                                             </div>
-                                            <div class="col-md-6">
-                                                <div class="form-group">
-                                                    <label for="gambar_barang">Gambar Barang</label>
-                                                    <input class="form-control" type="file" name="gambar_barang" placeholder="Gambar Barang" multiple="multiple"/>
-                                                </div>
+                                        <div class="col-md-6">
+                                            <div class="form-group">
+                                                <label for="gambar_barang">Gambar Barang</label>
+                                                <input class="form-control" type="file" name="gambar_barang" placeholder="Gambar Barang" multiple="multiple"/>
                                             </div>
-
-                                            <div class=" col-3">
-                                                <div class="form-group">
-                                                    <label for="Stock">Stock</label>
-                                                    <input class="form-control" type="number" name="Stock" placeholder="Stock" />
-                                                </div>
-                                            </div>
-
-                                            <div class=" col-3">
-                                                <div class="form-group">
-                                                    <label for="Stock">Diskon (%)</label>
-                                                    <input class="form-control" type="number" name="diskon" placeholder="%" />
-                                                </div>
-                                            </div>
-                                            
                                         </div>
+                                        <div class=" col-3">
+                                            <div class="form-group">
+                                                <label for="Stock">Stock</label>
+                                                <input class="form-control" type="number" name="Stock" placeholder="Stock" />
+                                            </div>
+                                        </div>
+                                        <div class=" col-3">
+                                            <div class="form-group">
+                                                <label for="Stock">Diskon (%)</label>
+                                                <input class="form-control" type="number" name="diskon" placeholder="%" />
+                                            </div>
+                                        </div>
+                                            
+                                    </div>
 
                                         <div class="row">
                                             <div class="col-md-12">
@@ -102,29 +98,27 @@
                                             </div>
                                         </div>
                                            
-                                        <label>Ukuran yang Tersedia</label>
-                                        <div class="custom-control custom-checkbox mb-3">
-                                            <input class="custom-control-input" id="s" type="checkbox">
-                                            <label class="custom-control-label" for="s">S</label>
-                                        </div>
-                                        <div class="custom-control custom-checkbox mb-3">
-                                            <input class="custom-control-input" id="m" type="checkbox">
-                                            <label class="custom-control-label" for="m">M</label>
-                                        </div>
-                                        <div class="custom-control custom-checkbox mb-3">
-                                            <input class="custom-control-input" id="l" type="checkbox">
-                                            <label class="custom-control-label" for="l">L</label>
-                                        </div>
-                                        <div class="custom-control custom-checkbox mb-3">
-                                            <input class="custom-control-input" id="xl" type="checkbox">
-                                            <label class="custom-control-label" for="xl">XL</label>
-                                        </div>
-                                        <div class="custom-control custom-checkbox mb-3">
-                                            <input class="custom-control-input" id="xxl" type="checkbox">
-                                            <label class="custom-control-label" for="xxl">XXL</label>
-                                        </div>
-
-
+                                            <label>Ukuran yang Tersedia</label>
+                                            <div class="custom-control custom-checkbox mb-3">
+                                                <input class="custom-control-input" id="s" type="checkbox">
+                                                <label class="custom-control-label" for="s">S</label>
+                                            </div>
+                                            <div class="custom-control custom-checkbox mb-3">
+                                                <input class="custom-control-input" id="m" type="checkbox">
+                                                <label class="custom-control-label" for="m">M</label>
+                                            </div>
+                                            <div class="custom-control custom-checkbox mb-3">
+                                                <input class="custom-control-input" id="l" type="checkbox">
+                                                <label class="custom-control-label" for="l">L</label>
+                                            </div>
+                                            <div class="custom-control custom-checkbox mb-3">
+                                                <input class="custom-control-input" id="xl" type="checkbox">
+                                                <label class="custom-control-label" for="xl">XL</label>
+                                            </div>
+                                            <div class="custom-control custom-checkbox mb-3">
+                                                <input class="custom-control-input" id="xxl" type="checkbox">
+                                                <label class="custom-control-label" for="xxl">XXL</label>
+                                            </div>
                                         
                                         <br />
                                         <?php if ($this->session->flashdata('success')) : ?>
@@ -132,29 +126,17 @@
                                             <?php echo $this->session->flashdata('success'); ?>
                                         </div>
                                         <?php endif; ?>
-                                    </form>
+                                </form>
 
-                                    </div>
-
-
-
-</div>
-
-</div>
-</div>
+                            </div>
+                    </div>
+                </div>
             </div>
-</div>
+        </div>
+    </div>
 
 </div>
 <!-- End of Main Content -->
-
-</div>
-</div>
-</div>
-</div>
-</div>
-</div>
-</div>
 <!-- Footer -->
         <!-- Footer -->
       <?php $this->load->view('admin/partials/footer.php'); ?>

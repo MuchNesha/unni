3e7e145058870b4ad5f5f43975a56c2be22fa0cc
much nesha adinata r <41299486+MuchNesha@users.noreply.github.com>
2019-12-19 13:05:19 +0700

    Update login.php

--- application/views/admin/login.php
+++ application/views/admin/login.php
@@ -6,7 +6,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <meta name="description" content="Start your development with a Dashboard for Bootstrap 4.">
   <meta name="author" content="Creative Tim">
-  <title>Login</title>
+  <title>Masuk</title>
   <!-- Favicon -->
   <link href="<?php echo base_url('assets/img/brand/icon.png'); ?>" rel="icon" type="image/png">
   <!-- Fonts -->

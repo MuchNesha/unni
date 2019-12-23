b469d08054386e0f360809ae81ecff890b204e4d
yosefyoga <47852108+yosefyoga@users.noreply.github.com>
2019-12-23 15:39:37 +0700

    Pembayaran

---
+++ application/views/pembayaran/pembayaran.php
@@ -0,0 +1,81 @@
+<div class="container">
+  <h3>DATA & ALAMAT PENERIMA</h3>
+  <div class="row">
+                <div class="col ml-1">
+                    <div class="text-top">
+                        <p>Isi data dengan benar! </p>
+                    </div>
+                </div>
+    </div>
+  <form>
+  <div class="form-row">
+  <div class="form-group col-md-6">
+			<label for="contoh1">Nama Lengkap</label>
+			<input type="text" class="form-control" id="contoh1" placeholder="ID">
+		</div>
+        <div class="form-group col-md-6">
+			<label for="contoh1">Provinsi</label>
+			<input type="text" class="form-control" id="contoh1" placeholder=" ">
+		</div>
+        </div>
+    <div class="form-row">
+                <div class="form-group col-md-6">
+                <label for="nama">Nomor Handphone </label>
+                    <input type="" name="" class="form-control" id="" placeholder="" data-rule=""
+                        data-msg="Please enter at least 4 chars" required/>
+                    <div class="validation"></div>
+                </div>
+                <div class="form-group col-md-6">
+                <label for="nama">Kota/Kabupaten</label>
+                    <input type="" class="form-control" name="" id="" placeholder="" data-rule=""
+                        data-msg="Please enter a valid email" required/>
+                    <div class="validation"></div>
+                </div>
+            </div>
+        
+	<div class="form-row">
+		<div class="form-group col-md-6">
+			<label for="contoh1">E-mail</label>
+			<input type="text" class="form-control" id="contoh1" placeholder="">
+		</div>
+		<div class="form-group col-md-6">
+			<label for="contoh2">Kecamatan</label>
+			<input type="text" class="form-control" id="contoh2" placeholder="">
+		</div>
+	</div>
+    <div class="form-row">
+		<div class="form-group col-md-6">
+			<label for="contoh1">Catatan Pengiriman</label>
+			<input type="text" class="form-control" id="contoh1" placeholder="">
+		</div>
+        <!-- <div class="form-row"> -->
+            <div class="form-group col-md-6">
+                <label for="contoh1">Kode Pos</label>
+                <input type="text" class="form-control" id="contoh1"placeholder="">
+            </div>
+		</div>                               
+             
+	<!-- </div> -->
+        </div>
+   
+
+   
+    <div class="text-center mb-3"><button type="submit" name="inputkk">Lanjut ke Pembayaran </button></div>
+  </form>
+</div>
+
+
+    <!-- jQuery (Necessary for All JavaScript Plugins) -->
+    <script src="<?php echo base_url(); ?>/assets/js/jquery/jquery-2.2.4.min.js"></script>
+    <!-- Popper js -->
+    <script src="<?php echo base_url(); ?>/assets/js/popper.min.js"></script>
+    <!-- Bootstrap js -->
+    <script src="<?php echo base_url(); ?>/assets/js/bootstrap.min.js"></script>
+    <!-- Plugins js -->
+    <script src="<?php echo base_url(); ?>/assets/js/plugins.js"></script>
+    <!-- Active js -->
+    <script src="<?php echo base_url(); ?>/assets/js/active.js"></script>
+
+</body>
+
+</html>

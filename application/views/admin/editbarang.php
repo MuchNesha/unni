e49acec3d83a27195994c4c6714dc7a0cad577e6
much nesha adinata r <41299486+MuchNesha@users.noreply.github.com>
2019-12-29 15:54:47 +0700

    edit

--- application/views/admin/editbarang.php
+++ application/views/admin/editbarang.php
@@ -1,147 +1,139 @@
 <?php $this->load->view('admin/partials/head.php'); ?>
 <?php $this->load->view('admin/partials/menu.php'); ?>
-    <div class="main-content">
-        <!-- Page content -->
-        <div class="header bg-gradient-primary pb-8 pt-5 pt-md-8">
-            <div class="container-fluid">
-                <div class="header-body">
-                    <h1 style="color:aliceblue">Tambah Barang</h1>
-                        <br>
-                        <h3 style="color:aliceblue">Admin bisa menambahkan barang dihalaman ini 😉</h3>
-                        <br>
-                    <!-- Start Container Fluid -->
-                    <div class="card bg-secondary shadow">
-                            <div class="card-header bg-white border-0">
-                                <div class="row align-items-center">
-                                    <div class="col-8">
-                                    <h3 class="mb-0">Tambah Barang</h3>
+<div class="main-content">
+    <!-- Page content -->
+    <div class="header bg-gradient-primary pb-8 pt-5 pt-md-8">
+        <div class="container-fluid">
+            <div class="header-body">
+                <h1 style="color:aliceblue">Tambah Barang</h1>
+                <br>
+                <h3 style="color:aliceblue">Admin bisa menambahkan barang dihalaman ini 😉</h3>
+                <br>
+                <!-- Start Container Fluid -->
+                <div class="card bg-secondary shadow">
+                    <div class="card-header bg-white border-0">
+                        <div class="row align-items-center">
+                            <div class="col-8">
+                                <h3 class="mb-0">Tambah Barang</h3>
+                            </div>
+                            <div class="col-4 text-right">
+                                <input class="btn btn-primary" type="submit" name="btn" value="Simpan" />
+                                <br />
+                                <?php if ($this->session->flashdata('success')) : ?>
+                                    <div class="alert alert-success" role="alert">
+                                        <?php echo $this->session->flashdata('success'); ?>
                                     </div>
-                                        <div class="col-4 text-right">
-                                        <input class="btn btn-primary" type="submit" name="btn" value="Simpan" />
-                                        <br />
-                                        <?php if ($this->session->flashdata('success')) : ?>
-                                            <div class="alert alert-success" role="alert">
-                                                <?php echo $this->session->flashdata('success'); ?>
-                                            </div>
-                                            <?php endif; ?>
-                                        </div>
+                                <?php endif; ?>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="card-body">
+                        <form action="<?php base_url('admin/databarang/add') ?>" method="post" enctype="multipart/form-data">
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <label for="id_barang">No Barang</label>
+                                        <input class="form-control" type="text" name="id_barang" value="<?php echo $barang->id_barang ?>" />
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <label for="form-control">Pilih kategori Barang</label>
+                                        <select class="form-control" id="kategoribarang">
+                                            <option>Hijab Instant</option>
+                                            <option>Hijab Segiempat</option>
+                                            <option>Baju Muslim</option>
+                                            <option>Aksesoris</option>
+                                        </select>
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <label for="harga">Harga Barang</label>
+                                        <input class="form-control" type="text" name="harga_brg" value="<?php echo $barang->harga_barang ?>" />
+                                    </div>
+                                </div>
+                                <div class="col-md-3">
+                                    <div class="form-group">
+                                        <label for="grosir">Harga Barang</label>
+                                        <input class="form-control" type="text" name="grosir" value="<?php echo $barang->harga_grosir ?>" />
+                                    </div>
+                                </div>
+                                <div class="col-md-3">
+                                    <div class="form-group">
+                                        <label for="per">Per</label>
+                                        <input class="form-control" type="text" name="per" value="<?php echo $barang->per ?>" />
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <label for="gambar">Gambar Barang</label>
+                                        <input class="form-control" type="file" name="gambar" placeholder="Gambar Barang" value="<?php echo $barang->gambar ?>" />
+                                    </div>
+                                </div>
+                                <div class=" col-3">
+                                    <div class="form-group">
+                                        <label for="stock">Stock Barang</label>
+                                        <input class="form-control" type="text" name="stock" placeholder="Harga" value="<?php echo $barang->stock ?>" />
+                                    </div>
+                                </div>
+                                <div class=" col-3">
+                                    <div class="form-group">
+                                        <label for="diskon">Diskon</label>
+                                        <input class="form-control" type="text" name="diskon" placeholder="%" value="<?php echo $barang->diskon ?>" />
+                                    </div>
+                                </div>
+
+                            </div>
+
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <div class="form-group">
+                                        <label for="deskripsi">Deskripsi Barang</label>
+                                        <input class="form-control" type="text" name="deskripsi" placeholder="Deskripsi" value="<?php echo $barang->deskripsi ?>" />
+                                    </div>
                                 </div>
                             </div>
-
-                            <div class="card-body">
-                                <form action="<?php base_url('admin/databarang/add') ?>" method="post" enctype="multipart/form-data">
-                                    <div class="row">
-                                        <div class="col-md-6">
-                                        <div class="form-group">
-                                                    <label for="id_barang">No Barang</label>
-                                                    <input class="form-control" type="text" name="id_barang"
-                                                        value="<?php echo $barang->id_barang ?>" />
-                                                </div>
-                                        </div>
-                                        <div class="col-md-6">
-                                            <div class="form-group">
-                                                <label for="form-control">Pilih kategori Barang</label>
-                                                <select class="form-control" id="kategoribarang">
-                                                <option>Hijab Instant</option>
-                                                <option>Hijab Segiempat</option>
-                                                <option>Baju Muslim</option>
-                                                <option>Aksesoris</option>
-                                                </select>
-                                            </div>
-                                        </div>
-                                        <div class="col-md-6">
-                                            <div class="form-group">
-                                                    <label for="harga">Harga Barang</label>
-                                                    <input class="form-control" type="text" name="harga_brg"
-                                                        value="<?php echo $barang->harga_barang ?>" />
-                                            </div>
-                                        </div>
-                                        <div class="col-md-3">
-                                        <div class="form-group">
-                                                    <label for="grosir">Harga Barang</label>
-                                                    <input class="form-control" type="text" name="grosir"
-                                                        value="<?php echo $barang->harga_grosir ?>" />
-                                            </div>
-                                        </div>
-                                            <div class="col-md-3">
-                                            <div class="form-group">
-                                                    <label for="per">Per</label>
-                                                    <input class="form-control" type="text" name="per"
-                                                        value="<?php echo $barang->per ?>" />
-                                            </div>
-                                            </div>
-                                        <div class="col-md-6">
-                                        <div class="form-group">
-                                                    <label for="gambar">Gambar Barang</label>
-                                                    <input class="form-control" type="file" name="gambar"
-                                                        placeholder="Gambar Barang" value="<?php echo $barang->gambar ?>" />
-                                                </div>
-                                        </div>
-                                        <div class=" col-3">
-                                        <div class="form-group">
-                                                    <label for="stock">Stock Barang</label>
-                                                    <input class="form-control" type="text" name="stock"
-                                                        placeholder="Harga" value="<?php echo $barang->stock ?>" />
-                                                </div>
-                                        </div>
-                                        <div class=" col-3">
-                                        <div class="form-group">
-                                                    <label for="diskon">Diskon</label>
-                                                    <input class="form-control" type="text" name="diskon"
-                                                        placeholder="%" value="<?php echo $barang->diskon ?>" />
-                                                </div>
-                                        </div>
-                                            
-                                    </div>
-
-                                        <div class="row">
-                                            <div class="col-md-12">
-                                                <div class="form-group">
-                                                    <label for="deskripsi">Deskripsi Barang</label>
-                                                    <input class="form-control" type="text" name="deskripsi"
-                                                        placeholder="Deskripsi" value="<?php echo $barang->deskripsi ?>" />
-                                                </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                           <div class="col-md-3">
-                                            <label>Ukuran yang Tersedia</label>
-                                            <div class="custom-control custom-checkbox mb-3">
-                                                <input class="custom-control-input" id="s" type="checkbox">
-                                                <label class="custom-control-label" for="s">S</label>
-                                            </div>
-                                            <div class="custom-control custom-checkbox mb-3">
-                                                <input class="custom-control-input" id="m" type="checkbox">
-                                                <label class="custom-control-label" for="m">M</label>
-                                            </div>
-                                            <div class="custom-control custom-checkbox mb-3">
-                                                <input class="custom-control-input" id="l" type="checkbox">
-                                                <label class="custom-control-label" for="l">L</label>
-                                            </div>
-                                            <div class="custom-control custom-checkbox mb-3">
-                                                <input class="custom-control-input" id="xl" type="checkbox">
-                                                <label class="custom-control-label" for="xl">XL</label>
-                                            </div>
-                                            <div class="custom-control custom-checkbox mb-3">
-                                                <input class="custom-control-input" id="xxl" type="checkbox">
-                                                <label class="custom-control-label" for="xxl">XXL</label>
-                                            </div>
-                                            </div>
-                                        
-                                        <br />
-                                        <?php if ($this->session->flashdata('success')) : ?>
-                                        <div class="alert alert-success" role="alert">
-                                            <?php echo $this->session->flashdata('success'); ?>
-                                        </div>
-                                        
-                                        <?php endif; ?>
-                                </form>
-                            </div>
                     </div>
+                    <div class="col-md-3">
+                        <label>Ukuran yang Tersedia</label>
+                        <div class="custom-control custom-checkbox mb-3">
+                            <input class="custom-control-input" id="s" type="checkbox">
+                            <label class="custom-control-label" for="s">S</label>
+                        </div>
+                        <div class="custom-control custom-checkbox mb-3">
+                            <input class="custom-control-input" id="m" type="checkbox">
+                            <label class="custom-control-label" for="m">M</label>
+                        </div>
+                        <div class="custom-control custom-checkbox mb-3">
+                            <input class="custom-control-input" id="l" type="checkbox">
+                            <label class="custom-control-label" for="l">L</label>
+                        </div>
+                        <div class="custom-control custom-checkbox mb-3">
+                            <input class="custom-control-input" id="xl" type="checkbox">
+                            <label class="custom-control-label" for="xl">XL</label>
+                        </div>
+                        <div class="custom-control custom-checkbox mb-3">
+                            <input class="custom-control-input" id="xxl" type="checkbox">
+                            <label class="custom-control-label" for="xxl">XXL</label>
+                        </div>
+                    </div>
+
+                    <br />
+                    <?php if ($this->session->flashdata('success')) : ?>
+                        <div class="alert alert-success" role="alert">
+                            <?php echo $this->session->flashdata('success'); ?>
+                        </div>
+
+                    <?php endif; ?>
+                    </form>
                 </div>
             </div>
         </div>
     </div>
+</div>
+</div>
 
 </div>
 <!-- End of Main Content -->
--- application/views/tentangkami/tentang.php
+++ application/views/tentangkami/tentang.php
@@ -20,62 +20,62 @@
 
         <!-- ****** Welcome Slides Area Start ****** -->
         <section class="welcome_area">
-                <!-- Single Slide Start -->
-                <div class="single_slide height-800 bg-img background-overlay" style="background-image: url(<?php echo base_url(); ?>/assets/img/bg-img/bg-1.jpg);">
-                    <div class="container h-100">
-                        <div class="row h-100 align-items-center">
-                            <div class="col-12">
-                                <div class="welcome_slide_text">
-                                    <h2 data-animation="fadeInUp" data-delay="500ms" data-duration="500ms">Tentang Kami</h2>
-                                </div>
+            <!-- Single Slide Start -->
+            <div class="single_slide height-800 bg-img background-overlay" style="background-image: url(<?php echo base_url(); ?>/assets/img/bg-img/bg-1.jpg);">
+                <div class="container h-100">
+                    <div class="row h-100 align-items-center">
+                        <div class="col-12">
+                            <div class="welcome_slide_text">
+                                <h2 data-animation="fadeInUp" data-delay="500ms" data-duration="500ms">Tentang Kami</h2>
                             </div>
                         </div>
                     </div>
+                </div>
 
- <!-- ****** New Arrivals Area Start ****** -->
- <section class="new_arrivals_area section_padding_100_0 clearfix">
-            <div class="container">
-                <div class="row">
-                    <div class="col-12">
-                        <div class="section_heading text-center">
-                            <h2>Tentang Kami</h2>
+                <!-- ****** New Arrivals Area Start ****** -->
+                <section class="new_arrivals_area section_padding_100_0 clearfix">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-12">
+                                <div class="section_heading text-center">
+                                    <h2>Tentang Kami</h2>
+                                </div>
+                            </div>
                         </div>
+                        <div class="row">
+                            <div class="col ml-5">
+                                <div class="text-center">
+                                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. A architecto ad consequatur hic deleniti reprehenderit laborum, quidem eius saepe sint quae debitis modi fugiat. Error eius quod nihil placeat? Consequatur.</p>
+                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi numquam sint quam possimus ab ut voluptatem quae. Explicabo labore, dolores consectetur quisquam veniam veritatis voluptates optio at molestias. Cumque, asperiores!</p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="offer_area height-700 section_padding_100">
+                                <h4 style="font-family: 'Nunito' ;  font-size: 25px;">Lokasi</h4>
+                                <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d1975.9247101804347!2d113.8228795!3d-7.9107943!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd6dce80e1e3687%3A0x64b9b0180d5a33ae!2sJl.%20KH%20Mansur%20No.3%2C%20Pegadaian%2C%20Blindungan%2C%20Kec.%20Bondowoso%2C%20Kabupaten%20Bondowoso%2C%20Jawa%20Timur%2068212!5e0!3m2!1sid!2sid!4v1576165250467!5m2!1sid!2sid" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
+                            </div>
+                        </div>
+
+
+                        <section class="offer_area height-700 section_padding_100 bg-img" style="background-image: url(<?php echo base_url(); ?>/assets/img/bg-img/bg-6.jpg);">
+                        </section>
+
                     </div>
-                </div>
-                <div class="row">
-                <div class="col ml-5">
-                    <div class="text-center">
-                         <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. A architecto ad consequatur hic deleniti reprehenderit laborum, quidem eius saepe sint quae debitis modi fugiat. Error eius quod nihil placeat? Consequatur.</p>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi numquam sint quam possimus ab ut voluptatem quae. Explicabo labore, dolores consectetur quisquam veniam veritatis voluptates optio at molestias. Cumque, asperiores!</p>
-                    </div>
-                </div>
-                </div>
-        
-                <div class="row">
-                <div class="offer_area height-700 section_padding_100">
-                    <h4 style="font-family: 'Nunito' ;  font-size: 25px;">Lokasi</h4>
-                    <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d1975.9247101804347!2d113.8228795!3d-7.9107943!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd6dce80e1e3687%3A0x64b9b0180d5a33ae!2sJl.%20KH%20Mansur%20No.3%2C%20Pegadaian%2C%20Blindungan%2C%20Kec.%20Bondowoso%2C%20Kabupaten%20Bondowoso%2C%20Jawa%20Timur%2068212!5e0!3m2!1sid!2sid!4v1576165250467!5m2!1sid!2sid" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
-                </div>
-                </div>
-                
 
-                <section class="offer_area height-700 section_padding_100 bg-img" style="background-image: url(<?php echo base_url(); ?>/assets/img/bg-img/bg-6.jpg);">
-        </section>
-                
-            </div>
-            
- 
-    <!-- jQuery (Necessary for All JavaScript Plugins) -->
-    <script src="<?php echo base_url(); ?>/assets/js/jquery/jquery-2.2.4.min.js"></script>
-    <!-- Popper js -->
-    <script src="<?php echo base_url(); ?>/assets/js/popper.min.js"></script>
-    <!-- Bootstrap js -->
-    <script src="<?php echo base_url(); ?>/assets/js/bootstrap.min.js"></script>
-    <!-- Plugins js -->
-    <script src="<?php echo base_url(); ?>/assets/js/plugins.js"></script>
-    <!-- Active js -->
-    <script src="<?php echo base_url(); ?>/assets/js/active.js"></script>
 
-</body>
+                    <!-- jQuery (Necessary for All JavaScript Plugins) -->
+                    <script src="<?php echo base_url(); ?>/assets/js/jquery/jquery-2.2.4.min.js"></script>
+                    <!-- Popper js -->
+                    <script src="<?php echo base_url(); ?>/assets/js/popper.min.js"></script>
+                    <!-- Bootstrap js -->
+                    <script src="<?php echo base_url(); ?>/assets/js/bootstrap.min.js"></script>
+                    <!-- Plugins js -->
+                    <script src="<?php echo base_url(); ?>/assets/js/plugins.js"></script>
+                    <!-- Active js -->
+                    <script src="<?php echo base_url(); ?>/assets/js/active.js"></script>
 
-</html>
+                    </body>
+
+                    </html>

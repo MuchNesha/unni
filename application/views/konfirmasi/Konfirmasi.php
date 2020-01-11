3d114cdc78df053643b5df9510c3b6eefd139f36
much nesha adinata r <41299486+MuchNesha@users.noreply.github.com>
2020-01-12 01:09:56 +0700

    segawon 😜😜😁👲

--- application/views/konfirmasi/Konfirmasi.php
+++ application/views/konfirmasi/Konfirmasi.php
@@ -1,102 +1,98 @@
-<link rel="stylesheet" href="style.css">
-<link rel="stylesheet" href="css/core-style.css">
-<link href="css/responsive.css" rel="stylesheet">
+<body>
+    
+    <div id="wrapper">
 
-<div class="container">
-            <div class="row  pt-2 pb-2">
-                <div class="col-3 md-3 pt-6 text-center">
-                    <button type="button" class="btn btn-danger">1</button>
-                    <a class="text-center" href="<?= site_url('keranjang'); ?>">keranjang belanja</a>
-                </div>
-                <div class="col-3 md-4 pt-6 text-center">
-                     <button type="button" class="btn btn-danger">2</button>
-                     <a class="text-center" href="<?= site_url('pengiriman'); ?>">pengiriman</a>
-                </div>
-                <div class="col-3 md-4 pt-6 text-center">
-                    <button type="button" class="btn btn-danger">3</button>
-                    <a class="text-center" href="<?= site_url('pembayaran'); ?>">pembayaran</a>
-                </div>
-                <div class="col-3 md-3 pt-6 float-right text-center">
-                    <button type="button" class="btn btn-danger">4</button>
-                    <a class="text-center" href="<?= site_url('konfirmasi'); ?>">konfirmasi</a>
-                </div>
+        <section class="top-discount-area d-md-flex align-items-center">
+            <!-- Single Discount Area -->
+            <div class="single-discount-area">
+                <h5>Free Shipping &amp; Returns</h5>
+                <h6><a href="#">BUY NOW</a></h6>
             </div>
-<br></br>
+            <!-- Single Discount Area -->
+            <div class="single-discount-area">
+                <h5>20% Discount for all dresses</h5>
+                <h6>USE CODE: Colorlib</h6>
+            </div>
+            <!-- Single Discount Area -->
+            <div class="single-discount-area">
+                <h5>20% Discount for students</h5>
+                <h6>USE CODE: Colorlib</h6>
+            </div>
+        </section>
 
-  <h3>SILAHKAN ISI DATA</h3>
-  <div class="row">
-                <div class="col ml-1">
-                    <div class="text-top">
-                        <p>Setelah transfer uang, isi ID pesanan dan konfirmasi transfer tadi agar barang belanjaan anda segera di proses dan dikirim </p>
+        <!-- ****** Checkout Area Start ****** -->
+        <div class="checkout_area section_padding_100">
+            <div class="container">
+                <div class="row">
+
+                    <div class="col-12 md-6">
+                        <div class="checkout_details_area mt-50 clearfix">
+
+                            <div class="row" y
+                            <div class="cart-page-heading">
+                                <h3>Silahkan isi data</h3>
+                                <p>Setelah transfer uang, isi ID pesanan dan konfirmasi transfer tadi agar barang belanjaan anda segera di proses dan dikirim</p>
+                            </div>
+
+                            <form action="<?php echo base_url('konformasi/add') ?>" method="post">
+                                <div class="row">
+                                    <div class="col-md-6 mb-3">
+                                        <label for="nama">ID Pesanan <span>*</span></label>
+                                        <input type="text" class="form-control" id="first_name" value="" required>
+                                    </div>
+                                    <div class="col-md-6 mb-3">
+                                        <label for="company">Tanggal Bayar</label>
+                                        <input type="date" class="form-control" id="company" value="">
+                                    </div>
+                                    <div class="col-md-6 mb-3">
+                                        <label for="postcode">Jumlah Bayar <span>*</span></label>
+                                        <input type="number" class="form-control" id="postcode" value="">
+                                    </div>
+                                    <div class="col-md-6 mb-3">
+                                    <label for="country">Metode Pembayaran <span>*</span></label>
+                                        <select class="custom-select d-block w-100" id="country">
+                                        <option value="transfer">Transfer ATM</option>
+                                        <option value="lain">Lain</option>
+                                    </select>
+                                    </div>
+                                    <div class="col-md-6 mb-3">
+                                        <label for="state">Dari Bank <span>*</span></label>
+                                        <input type="text" class="form-control" id="state" value="">
+                                    </div>
+                                    <div class="col-md-6 mb-3">
+                                        <label for="phone_number">Nama Pemilik Rekening <span>*</span></label>
+                                        <input type="text" class="form-control" id="phone_number" min="0" value="">
+                                    </div>
+                                    <div class="col-12 mb-4">
+                                        <label for="k">Unggah Bukti struk <span>*</span></label>
+                                        <input type="file" class="form-control" id="email_address" value="">
+                                    </div>
+
+                                    
+                                </div>
+                            </form>
+                        </div>
                     </div>
+
                 </div>
-    </div>
-  <form action="<?php echo base_url('konfirmasi/add') ?>" method="post">
-  <div class="form-row">
-  <div class="form-group col-md-6">
-			<label for="contoh1">ID Pesanan</label>
-			<input type="text" class="form-control" id="contoh1" name="id_pesanan" placeholder="">
-		</div>
+            </div>
         </div>
-    <div class="form-row">
-                <div class="form-group col-md-6">
-                <label for="nama">Tanggal Bayar :</label>
-                    <input type="" name="tgl_bayar" class="form-control" id="" placeholder="" data-rule=""
-                        data-msg="Please enter at least 4 chars" required/>
-                    <div class="validation"></div>
-                </div>
-                <div class="form-group col-md-6">
-                <label for="nama">jumlah bayar (Rp):</label>
-                    <input type="" class="form-control" name="jumlah_bayar" id="" placeholder="" data-rule=""
-                        data-msg="Please enter a valid email" required/>
-                    <div class="validation"></div>
-                </div>
-            </div>
+        <!-- ****** Checkout Area End ****** -->
+
         
-	<div class="form-row">
-		<div class="form-group col-md-6">
-			<label for="contoh1">Metode Pembayaran</label>
-			<input type="text" class="form-control" name="metode_pembayaran" id="contoh1" placeholder="">
-		</div>
-		<div class="form-group col-md-6">
-			<label for="contoh2">Dari Bank</label>
-			<input type="text" class="form-control" name="dari_bank" id="contoh2" placeholder="">
-		</div>
-	</div>
-    <div class="form-row">
-		<div class="form-group col-md-6">
-			<label for="contoh1">Nama Pemilik Rekening</label>
-			<input type="text" class="form-control" id="contoh1" name="nama_pemilik_rekening" placeholder="">
-		</div>
-        <div class="form-row">
-            <div class="form-group col-md-12">
-                <label for="contoh1">Unggah bukti Struk atau Lainnya</label>
-		            <div class="custom-file">
-		                <input type="file" class="custom-file-input" id="contohupload2">
-		                <label class="custom-file-label" for="contohupload2">Choose file</label>
-                     </div>
-                </div>
-	        </div>
-            </div>
-   
-
-   
-    <div class="text-center mb-3"><button type="submit" class="btn btn-danger" name="inputkk">Konfirmasi</button></div>
-  </form>
-</div>
-
-
+    </div>
+    <!-- /.wrapper end -->
 
     <!-- jQuery (Necessary for All JavaScript Plugins) -->
-    <script src="<?php echo base_url(); ?>/assets/js/jquery/jquery-2.2.4.min.js"></script>
+    <script src="js/jquery/jquery-2.2.4.min.js"></script>
     <!-- Popper js -->
-    <script src="<?php echo base_url(); ?>/assets/js/popper.min.js"></script>
+    <script src="js/popper.min.js"></script>
     <!-- Bootstrap js -->
-    <script src="<?php echo base_url(); ?>/assets/js/bootstrap.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
     <!-- Plugins js -->
-    <script src="<?php echo base_url(); ?>/assets/js/plugins.js"></script>
+    <script src="js/plugins.js"></script>
     <!-- Active js -->
-    <script src="<?php echo base_url(); ?>/assets/js/active.js"></script>
+    <script src="js/active.js"></script>
 
 </body>
 

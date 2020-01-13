81e5444f86474924540585315a20a9e6d125c2ab
waragafila <ajicheater@gmail.com>
2020-01-13 20:52:51 +0700

    pengiriman

--- application/controllers/pengiriman.php
+++ application/controllers/pengiriman.php
@@ -4,48 +4,30 @@ defined('BASEPATH') or exit('No direct script access allowed');
 class pengiriman extends CI_Controller
 {
 
-	function __construct()
-	{
-		parent::__construct();
-		$this->load->helper('url');
-		$this->load->model("model_pelanggan");
-	}
+    function __construct()
+    {
+        parent::__construct();
+        $this->load->helper('url');
+        $this->load->model("model_pelanggan");
+    }
 
-	public function index()
-	{
-		$data['pelanggan'] = $this->model_pelanggan->getAll();
-		$this->load->view('header');
-		$this->load->view("pengiriman/pengiriman", $data);
+    public function index()
+    {
+        $this->load->view('header');
+        $this->load->view("pengiriman/pengiriman");
         $this->load->view('footer');
-        $this->load->view('script');
-	}
+        // $this->load->view('script');
+    }
 
-	public function add()
-	{
-        $nama_pelanggan = $this->input->post('nama_pelanggan');
-        $no_hp_pelanggan = $this->input->post('no_hp_pelanggan');
-        $email_pelanggan = $this->input->post('email_pelanggan');
-        $provinsi = $this->input->post('provinsi');
-        $kabupaten = $this->input->post('kabupaten');
-        $kecamatan = $this->input->post('kecamatan');
-        $kode_pos = $this->input->post('kode_pos');
-        $data = array(
-            'nama_pelanggan' => $nama_pelanggan,
-            'no_hp_pelanggan' => $no_hp_pelanggan,
-            'email_pelanggan' => $email_pelanggan,
-            'provinsi' => $provinsi,
-            'kabupaten' => $kabupaten,
-            'kecamatan' => $kecamatan,
-            'kode_pos' => $kode_pos,
+    public function add()
+    {
 
-        );
-        $this->model_pelanggan->input_data($data, 'pelanggan');
+        $this->model_pelanggan->save();
         redirect('pengiriman');
-	}
+    }
 
-	public function input_data($data, $tabel)
+    public function input_data($data, $tabel)
     {
         $this->db->insert($tabel, $data);
     }
-
 }

bde5282497f8033c01835679f5073faf4a92f281
much nesha adinata r <41299486+MuchNesha@users.noreply.github.com>
2020-01-13 15:08:25 +0700

    fix bug

--- application/views/admin/editbarang.php
+++ application/views/admin/editbarang.php
@@ -73,11 +73,13 @@
                                 <div class="col-md-6">
                                     <div class="form-group">
                                         <label for="gambar">Gambar Barang</label>
-                                        <input class="form-control" type="file" name="gambar" placeholder="Gambar Barang" />
-                                        <input type="hidden" name="old-image" value="<?php echo $barang->gambar ?>">
+                                        <input class="form-control" type="file" name="gambar" placeholder="Gambar Barang" accept=".jpg, .jpeg, .png "/>
+                                        <input type="hidden" name="old-image" value="<?php echo $barang->gambar ?>" require/>
                                     </div>
                                     <div class="form-group">
                                         <img src="<?= base_url('assets/upload/produk/' . $barang->gambar) ?>" width="250px" height="300px">
+                                        <p><?php echo $barang->gambar ?></p>
+                                        
                                     </div>
                                 </div>
                                 <div class=" col-3">
